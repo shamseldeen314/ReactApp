d4d575a50c4d04e144832b68be565590b0fc3a21
shamseldeen314 <yshamseldeen314@gmail.com>
2022-09-03 22:19:49 +0200

    data file for parent component

---
+++ src/BirthDayCard/Data/ParentData.js
@@ -0,0 +1,29 @@
+export default[
+    {
+        id: 1,
+        name: 'mohamed sobhy',
+        address: 'kamal ramzy',
+        age: 56,
+        phone: '01236521589',
+        gender: 'male'
+
+    },
+    {
+        id: 2,
+        name: 'walid sobhy',
+        address: 'kamal ramzy',
+        age: 44,
+        phone: '01254879562',
+        gender: 'male'
+
+    },
+    {
+        id: 3,
+        name: 'saleh sobhy',
+        address: 'kamal ramzy',
+        age: 38,
+        phone: '0125896314',
+        gender: 'male'
+    },
+]
+
