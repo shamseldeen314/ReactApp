9cf875beed84c9368526c21156b484ed6a0d0bdf
shamseldeen314 <yshamseldeen314@gmail.com>
2022-09-03 22:19:07 +0200

    data file for kids component

---
+++ src/BirthDayCard/Data/ChildData.js
@@ -0,0 +1,60 @@
+import Laiaaly from "../Images/Laiaaly.jpg";
+export default[
+    {
+        id: 1,
+        name: "Nour Sobhy",
+        age: 11,
+        gender: 'female',
+        image: Laiaaly
+
+
+        
+    },
+    {
+        id: 2,
+        name: "Basmala Yasser",
+        age: 8,
+        gender: 'female',
+        image: Laiaaly
+
+    },
+    {
+        id: 3,
+        name: "Shams Sobhy",
+        age: 7,
+        gender: 'Male',
+        image: Laiaaly
+
+    },
+    {
+        id: 4,
+        name: "Dohy Sobhy",
+        age: 7,
+        gender: 'female',
+        image: Laiaaly
+
+    },
+ 
+    {
+        id: 5,
+        name: "Liaaly Sobhy",
+        age: 5,
+        gender: 'female',
+        image: Laiaaly
+
+    },   {
+        id: 6,
+        name: "Haya Yasser",
+        age: 4,
+        gender: 'female',
+        image: Laiaaly
+
+    },   {
+        id: 7,
+        name: "Hour Yasser",
+        age: 5,
+        gender: 'female',
+        image: Laiaaly
+
+    },
+]
