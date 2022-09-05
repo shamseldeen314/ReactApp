71180df971adccfe6deeef96b2b2466d0fc30295
shamseldeen314 <yshamseldeen314@gmail.com>
2022-09-05 12:44:37 +0200

    add child component

---
+++ src/BirthDayCard/Js/Family/ChildCard.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import '../../Css/BirthDay.css';
+import Data2 from '../../Data/ChildData'
+import ChildList from './ChildList';
+function ChildCard() {
+    const [children, setChildren] = useState(Data2)
+    const [state, setState] = useState(false);
+    const blockCard = () => {
+        setState(() => !state);
+    };
+    return (
+        <section className='container'>
+            <h3>{children.length} birthdays today</h3>
+            <div className={children.length > 0 ? " Card" : "Card_height_0"} >
+                <ChildList children={children} />
+            </div>
+            <div className='group_btn_card'>
+                <button className='clear_btn' onClick={() => setChildren([])}>clear children</button>
+                <button className='refresh_btn' onClick={() => setChildren(Data2)}>children</button>
+            </div>
+        </section>
+    )
+}
+export default ChildCard
